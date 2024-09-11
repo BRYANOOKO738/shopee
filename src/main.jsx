c85f24e37cb8832b9e8dda18aa62a5773111d493
Bryan Onyango <onyangobryan8@gmail.com>
2024-09-11 13:58:26 -0700

    FINALL

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux"; // Correctly import Provider
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./Redux/Store"; // Ensure correct import path for store
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
